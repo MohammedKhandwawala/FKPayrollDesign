9b2d4e2748e555621306a9fef2adac553d4bf05a
mak_52 <ee16b117@ee.iitm.ac.in>
2020-05-10 09:43:31 +0530

    Modified NewEmployeeTransaction.java 	-Added employee type to constructor

--- SourceCode/paymentsys/NewEmployeeTransaction.java
+++ SourceCode/paymentsys/NewEmployeeTransaction.java
@@ -6,12 +6,14 @@ public abstract class NewEmployeeTransaction {
 	private String name;
 	private boolean inUnion;
 	private Database db;
+	private String empType;
 
-	public NewEmployeeTransaction (int employee_ID,String name,boolean inUnion,Database db){
+	public NewEmployeeTransaction (int employee_ID,String name,boolean inUnion,String empType,Database db){
 		this.employee_ID = employee_ID;
     this.name = name;
     this.db = db;
     this.inUnion = inUnion;
+    this.empType = empType;
 	}
 
 	public abstract Payment CreatePayment();
@@ -21,6 +23,7 @@ public abstract class NewEmployeeTransaction {
 		Employee emp = new Employee(employee_ID,name);
 		emp.empPay = pt;
 		emp.inUnion = inUnion; 
+		emp.emptype = empType;
 		db.addEmployee(employee_ID,emp);
 	}
 }
