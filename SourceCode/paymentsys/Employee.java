f23a948e9eb4588dd01406d2f4e86b5553bdb1df
mak_52 <ee16b117@ee.iitm.ac.in>
2020-05-10 07:51:51 +0530

    Modified Employee.java 	-Added new attributes to Employee - PayDate , inUnion

--- SourceCode/paymentsys/Employee.java
+++ SourceCode/paymentsys/Employee.java
@@ -1,12 +1,15 @@
 package paymentsys;
+import java.time.LocalDateTime;
 
 public class Employee {
 	public String Name;
 	public int employee_ID;
 	public Payment empPay;
 	public boolean inUnion;
-	public double payDate;
+	public LocalDateTime payDate;
 	public double AccountBalance;
+	public int PaymentMode;
+	public String emptype;
 
 	public Employee(int empId, String name){
 		this.employee_ID = empId;
