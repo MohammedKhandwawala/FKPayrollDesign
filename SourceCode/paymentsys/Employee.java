c441e9956d6cfec36808021f39e85eb971536900
mak_52 <ee16b117@ee.iitm.ac.in>
2020-05-10 09:40:21 +0530

    Modified Employee java 	-Added caller methods for employee attributes

--- SourceCode/paymentsys/Employee.java
+++ SourceCode/paymentsys/Employee.java
@@ -14,6 +14,65 @@ public class Employee {
 	public Employee(int empId, String name){
 		this.employee_ID = empId;
 		this.Name = name;
+		this.PaymentMode = 2;
+		this.AccountBalance = 0;
+	}
+	public String getName(){
+		return this.Name;
+	}
+	public void setPayDate(LocalDateTime date){
+		this.payDate = date; 
+	}
+	public LocalDateTime getPayDate(){
+		return this.payDate;
+	}
+	public String getEmployeeType(){
+		return this.emptype;
+	}
+	public void updateSalary(){
+		if(this.emptype == "Salaried"){
+			MonthlyPayment p1 = (MonthlyPayment) this.empPay;
+			double money = p1.calcPay(this.payDate);
+			this.AccountBalance=+money;
+			//System.out.println("Employee ID : ",this.employee_ID," got salary ",money);
+			this.payDate = this.payDate.plusMonths(1);
+		}
+		else if(this.emptype == "Commision"){
+			CommissionedPayment p2 = (CommissionedPayment) this.empPay;
+			double money = p2.calcPay(this.payDate); 
+			this.AccountBalance =+ money;
+			this.payDate = this.payDate.plusDays(14);
+		}
+		else if(this.emptype == "Hourly"){
+			HourlyPayment p3 = (HourlyPayment) this.empPay;
+			double money = p3.calcPay(this.payDate);
+			this.AccountBalance=+money;
+			//System.out.println("Employee ID : ",this.employee_ID," got salary ",money);
+			this.payDate = this.payDate.plusDays(7);
+		}
+	}
+
+	public double getAccountBalance(){
+		return this.AccountBalance;
+	}
+	public boolean inUnion(){
+		return this.inUnion;
+	}
+
+	public void joinUnion(){
+		this.inUnion = true;
+	}
+
+	public void leaveUnion(){
+		this.inUnion = false;
+	}
+
+	public int getPaymentMode(){
+		return PaymentMode;
+	}
+
+	public void setPaymentMode(int mode){
+		this.PaymentMode = mode;
 	}
 }
 
